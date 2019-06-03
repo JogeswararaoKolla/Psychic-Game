9bd10dd340c2058e5cd6638127cac64256d0b72e
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-06-03 16:57:24 -0700

    Removed unnecessary logging and added functionality to check user key press is character
    Removed unnecessary logging and added functionality to check user key press is character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,70 @@
 let Psychic = [];
 let i = 0,
-    idx=0;
-    wins = 0,
-    guessLeft=0,
-    loses = 0;
+  idx = 0;
+wins = 0,
+  guessLeft = 0,
+  loses = 0;
 let guessSoFar = [];
 let computerChoice;
-let gamestart=false;
-
-  CreatePsychicGameList();
-
-  gameStart();
-
-  function gameStart(){
-// Slecting the random letter
-  guessLeft=10;
-  guessSoFar = [];
-  document.getElementById("guessesLeft").innerText=guessLeft; 
-  idx=Math.trunc(Math.random()*26);
-  console.log("Random index selected " + idx);
-  computerChoice=Psychic[idx];
-  console.log("Computer Choice Letter " + computerChoice);
-  }
-
-
-document.onkeyup=function(event){
-  console.log(String.fromCharCode(event.keyCode));
-  let user_key=String.fromCharCode(event.keyCode);
-     
-
-  if(guessLeft!=0)
-  {
-    if(computerChoice!=user_key)
-  {   guessLeft--;
-     guessSoFar.push(user_key);
-     console.log("Guesses Left: " + guessLeft);
-     document.getElementById("guessesLeft").innerText=guessLeft; 
-     console.log(guessSoFar.join());
-     document.getElementById("guessesSoFar").innerText=guessSoFar.join();
-  }
-  else 
-  {    wins++;
-      console.log("Hey Match the Letters");
-      document.getElementById("wins").innerText=wins;
-      gameStart();
-  }
-}
-else 
-{    loses++;
-console.log("Hey Game Over Guess left zero");
-document.getElementById("losses").innerText=loses;
-document.getElementById("guessesSoFar").innerText=' ';
-console.log("New game gettig started:");
-gameStart();
-}
-
-}
+let gamestart = false;
 
 //creating the letters 
 function CreatePsychicGameList() {
-    for (i = 65; i < 91; i++) {
-        let result = String.fromCharCode(i);
-        Psychic.push(result);
-    }
-   console.log(Psychic.length,Psychic);
+  for (i = 65; i < 91; i++) {
+    let result = String.fromCharCode(i);
+    Psychic.push(result);
+  }
+  console.log(Psychic.length, Psychic);
 }
+
+function gameStart() {
+  // Slecting the random letter
+  guessLeft = 10;
+  guessSoFar = [];
+  document.getElementById("guessesLeft").innerText = guessLeft;
+  idx = Math.trunc(Math.random() * 26);
+  computerChoice = Psychic[idx];
+  console.log("Computer Choice Letter " + computerChoice);
+}
+
+// main methods are being called here 
+CreatePsychicGameList();
+gameStart();
+
+document.onkeyup = function (event) {
+  console.log((event));
+  let user_key = String.fromCharCode(event.keyCode);
+
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    console.log("valid key entered:", event);
+
+    if (guessLeft != 0) {
+      if (computerChoice != user_key) {
+        guessLeft--;
+        guessSoFar.push(user_key);
+        document.getElementById("guessesLeft").innerText = guessLeft;
+        document.getElementById("guessesSoFar").innerText = guessSoFar.join();
+      }
+      else {
+        wins++;
+        document.getElementById("wins").innerText = wins;
+        gameStart();
+      }
+    }
+    else {
+      loses++;
+      document.getElementById("losses").innerText = loses;
+      document.getElementById("guessesSoFar").innerText = ' ';
+      gameStart();
+    }
+
+  }
+  else {
+    alert("Not a valid character entered!");
+  }
+}
+
+
 
 
 
